13d2dd7ddc8ce03e86aac5aade4bfa32d13e62b2
ArturoPerez7904 <arturoperez7904@gmail.com>
2023-12-05 22:10:29 -0600

    Created test selection and button modifiers. Also created quitting check to ensure writing to CSV file works properly

--- Assets/AnswerController.cs
+++ Assets/AnswerController.cs
@@ -3,25 +3,53 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 using System;
+using UnityEngine.XR.Interaction.Toolkit.Utilities.Tweenables;
 
 public class AnswerController : MonoBehaviour
 {
 
     string filename="";
+    public GameObject starter;
+    public GameObject testSelector;
     public List<Button> buttons = new List<Button>{};
+    public List<Button> testButtons = new List<Button>{};
     private int currentAnswer;
     List<int> answers = new List<int>{};
     List<double> degreeOffsetList = new List<double>{};
     List<float> timeToAnswerList = new List<float>{};
-    [SerializeField] List<int> correctAnswers = new List<int>{};
     private float timeToAnswer;
     System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
     public GameObject[] objs;
-    public GameObject starter;
-    [SerializeField] private float[] timeStops;
-    [SerializeField] private float stopTime;
+    [SerializeField] List<int> correctAnswers = new List<int>{};
+    [SerializeField] List<float> timeStops = new List<float>{};
     private int answerCounter = 0;
-    
+    public TestAnswerList ListOfTestAnswers = new TestAnswerList();
+    [System.Serializable]
+    public class TestAnswers
+    {
+        public List<int> theseAnswers;
+    }
+ 
+    [System.Serializable]
+    public class TestAnswerList
+    {
+        public List<TestAnswers> answersList;
+    }
+
+    public StopTimesList ListOfStopTimes = new StopTimesList();
+    [System.Serializable]
+    public class StopTimes
+    {
+        public List<int> theseStopTimes;
+    }
+ 
+    [System.Serializable]
+    public class StopTimesList
+    {
+        public List<StopTimes> stopTimesList;
+    }
+    TextWriter twTrue;
+    bool twTrueIsOpen = false;
 
     private void Awake()
         {
@@ -34,17 +62,35 @@ public class AnswerController : MonoBehaviour
         objs = GameObject.FindGameObjectsWithTag("Selectable");
         DeactivateAllButtons();
         AddStartListener();
+        GameObject.FindGameObjectWithTag("Start").transform.localScale = new Vector3(0, 0, 0);
         
+    }
+
+    void Quit()
+    {
+        if(twTrueIsOpen){
+            twTrue.Close();
+        }
+    }
+
+    [RuntimeInitializeOnLoadMethod]
+    void RunOnStart()
+    {
+        Application.quitting += Quit;
     }
 
     private void CreateFile()
     {
         int i = 1;
-        filename = Application.dataPath + "/" + i + "test.csv";
+        filename = Application.persistentDataPath + "/" + i + "test.csv";
         while (File.Exists(filename)){
-            filename = Application.dataPath + "/" + i + "test.csv";
+            filename = Application.persistentDataPath + "/" + i + "test.csv";
             i++;
         }
+        TextWriter twFalse = new StreamWriter(filename, false);
+        twFalse.WriteLine("Sentence Pair, Correct Answer, Given Answer, Time to Answer, Degree Offset");
+        twFalse.Close();
+        
     }
 
     void BeginTest(){
@@ -66,7 +112,6 @@ public class AnswerController : MonoBehaviour
 
     void GiveFinalAnswer(){
         answers.Add(currentAnswer);
-        int correctAnswer = correctAnswers[answerCounter];
         if (currentAnswer == 0){
             degreeOffsetList.Add(0);
             timeToAnswerList.Add(0);
@@ -80,21 +125,46 @@ public class AnswerController : MonoBehaviour
             timeToAnswerList.Add(timeToAnswer);
 
         }
-
-        //Change this to when test is stopped
-        if(answers.Count == 11){
-            WriteCSV();
-            Debug.Log("Test created");
+        if(answerCounter == 0){
+            twTrue = new StreamWriter(filename, true);
+            twTrueIsOpen = true;
         }
+
+        int row = answerCounter;
+
+        if((row%5 == 0) & (row != 0)){
+                twTrue.WriteLine("Passage Break");
+            }
+            
+        twTrue.WriteLine((row+1) + "," + correctAnswers[row] + "," + answers[row] + "," + timeToAnswerList[row] + "," + degreeOffsetList[row]);
+        twTrue.Flush();
         answerCounter++;
-	}
- 
-        private void AnswerListener(int index)
-        {
-            currentAnswer = index + 1;
-            timeToAnswer = stopwatch.ElapsedMilliseconds;
+        if(answerCounter >= correctAnswers.Count){
+            twTrue.Close();
+            twTrueIsOpen = false;
         }
+	}
+    void WriteCSV()
+    {
+        TextWriter tw = new StreamWriter(filename, false);
+        tw.WriteLine("Sentence Pair, Correct Answer, Given Answer, Time to Answer, Degree Offset");
+        tw.Close();
 
+        tw = new StreamWriter(filename, true);
+        int row = 0;
+        foreach (int answer in answers){
+            
+            if((row%5 == 0) & (row != 0)){
+                tw.WriteLine("Passage Break");
+            }
+            
+            tw.WriteLine((row+1) + "," + correctAnswers[row] + "," + answers[row] + "," + timeToAnswerList[row] + "," + degreeOffsetList[row]);
+
+            row++;
+        }
+        tw.Close();
+    }
+ 
     public void DeactivateAllButtons()
 	{
         stopwatch.Reset();
@@ -117,12 +187,25 @@ public class AnswerController : MonoBehaviour
         }
     }
 
+        private void AnswerListener(int index)
+        {
+            currentAnswer = index + 1;
+            timeToAnswer = stopwatch.ElapsedMilliseconds;
+        }
     void AddListeners()
     {
         int i = 0;
         foreach (Button btn in buttons)
         {
             ListenerHandler(btn, i);
+
+            i++;
+        }
+
+        i = 1;
+        foreach (Button btn in testButtons)
+        {
+            TestListenerHandler(btn, i);
 
             i++;
         }
@@ -133,31 +216,39 @@ public class AnswerController : MonoBehaviour
             btn.onClick.AddListener(() => { AnswerListener(index); });
         }
 
-    void WriteCSV()
-    {
-        TextWriter tw = new StreamWriter(filename, false);
-        tw.WriteLine("Sentence Pair, Correct Answer, Given Answer, Time to Answer, Degree Offset");
-        tw.Close();
-
-        tw = new StreamWriter(filename, true);
-        int row = 0;
-        foreach (int answer in answers){
-            
-            if((row%5 == 0) & (row != 0)){
-                tw.WriteLine("Passage Break");
-            }
-            
-            tw.WriteLine((row+1) + "," + correctAnswers[row] + "," + answers[row] + "," + timeToAnswerList[row] + "," + degreeOffsetList[row]);
-
-            row++;
+    private void TestListenerHandler(Button btn, int index)
+        {
+            btn.onClick.AddListener(() => { TestSelectionListener(index); });
         }
-        tw.Close();
-    }
+
+
     private void AddStartListener()
     {
         starter = GameObject.FindGameObjectWithTag("Start");
+        testSelector = GameObject.FindGameObjectWithTag("Test Selector");
         Button btn = starter.GetComponentInChildren<Button>();
         btn.onClick.AddListener(BeginTest);
     }
+    private void LoadTest(int selectedTest)
+    {
+        correctAnswers.Clear();
+        timeStops.Clear();
+        foreach (int answer in ListOfTestAnswers.answersList[selectedTest].theseAnswers){
+            correctAnswers.Add(answer);
+        }
+
+        foreach (int stopTime in ListOfStopTimes.stopTimesList[selectedTest].theseStopTimes){
+            timeStops.Add(stopTime);
+        }
+        Destroy(testSelector);
+        GameObject.FindGameObjectWithTag("Start").transform.localScale = new Vector3((float)0.3, (float)0.3, (float)0.3);
+
+    }
+
+    private void TestSelectionListener(int index)
+        {
+            int selectedTest = index;
+            LoadTest(selectedTest - 1);
+        }
 
 }
